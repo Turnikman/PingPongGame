f01f74c9757be159c6af6212297eaa71332bd554
Aleksey <turnikman.als@gmail.com>
2014-05-27 22:17:22 +0300

    files

---
+++ js/bot.js
@@ -0,0 +1,57 @@
+﻿// движение оппонента
+
+function aiMove() {
+    var y;
+    // делаем скорость оппонента зависимой от скорости шарика
+    switch (ball.vY) {
+        case 2:
+            vY = 2;
+            break;
+        case 3:
+            vY = 3;
+            break;
+        case 4:
+            vY = 4;
+            break;
+        case 5:
+            vY = 5;
+            break;
+        case 6:
+            vY = 5;
+            break;
+        case 7:
+            vY = 6;
+            break;
+        case 8:
+            vY = 6;
+            break;
+        case 9:
+            vY = 6;
+            break;
+        case 10:
+            vY = 7;
+            break;
+        case 11:
+            vY = 7;
+            break;
+        case 12:
+            vY = 7;
+            break;
+        case 13:
+            vY = 7;
+            break;
+        case 0:
+            vY = 0;
+            break;
+    }
+
+    if (ball.y < ai.y + ai.height / 2) {
+        y = ai.y - vY;
+    }
+    if (ball.y > ai.y + ai.height / 2) {
+        y = ai.y + vY;
+    }
+    if (10 < y && y < game.height - ai.height - 10) {
+        ai.y = y;
+    }
+}
---
+++ js/game.js
@@ -0,0 +1,42 @@
+﻿// отрисовка игры
+
+function draw() {
+    game.draw(); // игровое поле
+    // разделительная полоса
+    for (var i = 10; i < game.height; i += 45) {
+        context.fillStyle = "#ccc";
+        context.fillRect(game.width / 2 - 10, i, 20, 30);
+    }
+    // рисуем на поле счёт
+    context.font = 'bold 128px courier';
+    context.textAlign = 'center';
+    context.textBaseline = 'top';
+    context.fillStyle = '#ccc';
+    context.fillText(ai.scores, 100, 0);
+    context.fillText(player.scores, game.width - 100, 0);
+    ai.draw(); // левая ракетка
+    player.draw(); // ракетка игрока
+    ball.draw(); // шарик
+    /*
+    if (!start) {
+        // вывод статстики
+        context.fillStyle = "#ccc";
+        context.globalAlpha = 0.7;
+        context.fillRect(0, 0, game.width, game.height);
+        context.font = 'bold 16px courier';
+        context.textBaseline = 'top';
+        context.fillStyle = '#000';
+        context.fillText("Total: " + game.total + " Win: " + game.win + " Lose: " + game.lose, game.width / 2, 0);
+        context.font = 'bold 60px courier';
+        context.textBaseline = 'top';
+        context.fillStyle = '#000';
+        context.fillText("Pong", game.width / 2, game.height / 2 - 50);
+        context.font = 'bold 16px courier';
+        context.textBaseline = 'top';
+        context.fillStyle = '#000';
+        context.fillText("click on me", game.width / 2, game.height / 2 + 25);
+        context.textBaseline = 'bottom';
+
+    }
+    */
+}
